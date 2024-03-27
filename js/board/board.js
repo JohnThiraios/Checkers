a38b037f3865261e24eccbe2300114964202384d
mehrain <marijnrovers@live.nl>
2024-03-27 16:58:20 +0100

    need to fix the rendering of the board

--- js/board/board.js
+++ js/board/board.js
@@ -1,13 +1,15 @@
-const cells = document.querySelectorAll('.cell');
-
-cells.forEach(cell => {
-  const cellId = parseInt(cell.dataset.cellId, 10);
-  if (cellId % 2 === 0) {
-    cell.classList.add('even');
-  } else {
-    cell.classList.add('odd');
-  }
-});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const cells = document.querySelectorAll('.cell');
+  
+    cells.forEach(cell => {
+      const cellId = parseInt(cell.dataset.cellId, 10);
+      if (cellId % 2 === 0) {
+        cell.classList.add('even');
+      } else {
+        cell.classList.add('odd');
+      }
+    });
+  });
 /* Test clicker to display cell ID's and Class */
 document.addEventListener('click', function(event) {
     console.log('You clicked on element with id: ' + event.target.id);
